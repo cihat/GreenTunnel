ee2b6d6e7e3add978b5b6a0c2e40f47c60e37e7a
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2025-06-06 10:03:04 +0300

    Lint

--- gui/main.js
+++ gui/main.js
@@ -9,205 +9,205 @@ const os = require('os');
 const electron = require('electron');
 const dialog = electron.dialog;
 dialog.showErrorBox = function (title, content) {
-    console.log(`${title}\n${content}`);
+  console.log(`${title}\n${content}`);
 };
 
 // Handle Squirrel events for Windows installer
 const setupEvents = require('./installers/windows/setupEvents');
 if (setupEvents.handleSquirrelEvent()) {
-    return;
+  return;
 }
 
 let win, tray, proxy;
 let isOn = false;
 
 const menuItems = [
-    {
-        label: 'Turn Off',
-        type: 'normal',
-        click: () => turnOff(),
-    },
-    {
-        label: 'Run At Login',
-        type: 'checkbox',
-    },
-    {
-        type: 'separator',
-    },
-    {
-        label: 'Source Code',
-        type: 'normal',
-        click: () => shell.openExternal('https://github.com/SadeghHayeri/GreenTunnel'),
-    },
-    {
-        label: 'Donate',
-        type: 'normal',
-        click: () => shell.openExternal('https://github.com/SadeghHayeri/GreenTunnel#donation'),
-    },
-    {
-        role: 'quit',
-        label: 'Quit',
-        type: 'normal',
-    },
+  {
+    label: 'Turn Off',
+    type: 'normal',
+    click: () => turnOff(),
+  },
+  {
+    label: 'Run At Login',
+    type: 'checkbox',
+  },
+  {
+    type: 'separator',
+  },
+  {
+    label: 'Source Code',
+    type: 'normal',
+    click: () => shell.openExternal('https://github.com/SadeghHayeri/GreenTunnel'),
+  },
+  {
+    label: 'Donate',
+    type: 'normal',
+    click: () => shell.openExternal('https://github.com/SadeghHayeri/GreenTunnel#donation'),
+  },
+  {
+    role: 'quit',
+    label: 'Quit',
+    type: 'normal',
+  },
 ];
 
 async function turnOff() {
-    isOn = false;
+  isOn = false;
 
-    if (proxy) {
-        await proxy.stop();
-        proxy = null;
-    }
+  if (proxy) {
+    await proxy.stop();
+    proxy = null;
+  }
 
-    if (win && !win.isDestroyed()) {
-        win.webContents.send('changeStatus', isOn);
-    }
+  if (win && !win.isDestroyed()) {
+    win.webContents.send('changeStatus', isOn);
+  }
 
-    menuItems[0].label = 'Enable';
-    menuItems[0].click = () => turnOn();
-    tray.setContextMenu(Menu.buildFromTemplate(menuItems));
+  menuItems[0].label = 'Enable';
+  menuItems[0].click = () => turnOn();
+  tray.setContextMenu(Menu.buildFromTemplate(menuItems));
 
-    const iconPath = path.join(__dirname, 'images/iconDisabledTemplate@2x.png');
-    const trayIcon = nativeImage.createFromPath(iconPath);
-    tray.setImage(trayIcon);
+  const iconPath = path.join(__dirname, 'images/iconDisabledTemplate@2x.png');
+  const trayIcon = nativeImage.createFromPath(iconPath);
+  tray.setImage(trayIcon);
 }
 
 async function turnOn() {
-    isOn = true;
+  isOn = true;
 
-    if (proxy) {
-        await turnOff();
-    }
+  if (proxy) {
+    await turnOff();
+  }
 
-    proxy = new Proxy({ source: 'GUI' });
-    await proxy.start({ setProxy: true });
+  proxy = new Proxy({ source: 'GUI' });
+  await proxy.start({ setProxy: true });
 
-    if (win && !win.isDestroyed()) {
-        win.webContents.send('changeStatus', isOn);
-    }
+  if (win && !win.isDestroyed()) {
+    win.webContents.send('changeStatus', isOn);
+  }
 
-    menuItems[0].label = 'Disable';
-    menuItems[0].click = () => turnOff();
-    tray.setContextMenu(Menu.buildFromTemplate(menuItems));
+  menuItems[0].label = 'Disable';
+  menuItems[0].click = () => turnOff();
+  tray.setContextMenu(Menu.buildFromTemplate(menuItems));
 
-    const iconPath = path.join(__dirname, 'images/iconTemplate@2x.png');
-    const trayIcon = nativeImage.createFromPath(iconPath);
-    tray.setImage(trayIcon);
+  const iconPath = path.join(__dirname, 'images/iconTemplate@2x.png');
+  const trayIcon = nativeImage.createFromPath(iconPath);
+  tray.setImage(trayIcon);
 }
 
 function createWindow() {
-    const iconPath = path.join(__dirname, 'icons/icon.icns');
-    const appIcon = nativeImage.createFromPath(iconPath);
+  const iconPath = path.join(__dirname, 'icons/icon.icns');
+  const appIcon = nativeImage.createFromPath(iconPath);
 
-    const stateManager = windowStateKeeper();
+  const stateManager = windowStateKeeper();
 
-    win = new BrowserWindow({
-        width: 300,
-        height: 300,
-        x: stateManager.x,
-        y: stateManager.y,
-        maximizable: false,
-        minimizable: true,
-        fullscreenable: false,
-        resizable: false,
-        icon: appIcon,
-        show: false,
+  win = new BrowserWindow({
+    width: 300,
+    height: 300,
+    x: stateManager.x,
+    y: stateManager.y,
+    maximizable: false,
+    minimizable: true,
+    fullscreenable: false,
+    resizable: false,
+    icon: appIcon,
+    show: false,
 
-        title: 'Green Tunnel',
-        frame: false,
-        transparent: true,
-        webPreferences: {
-            // Keep original functionality - minimal changes for compatibility
-            nodeIntegration: true,
-            contextIsolation: false,
-            enableRemoteModule: false
-        }
-    });
-
-    // Save window state
-    stateManager.manage(win);
-
-    win.loadFile('./view/main-page/index.html');
-
-    win.once('ready-to-show', () => {
-        win.show();
-        win.focus();
-        turnOn();
-    });
-
-    win.on('closed', () => {
-        win = null;
-    });
-
-    if (debug) {
-        win.webContents.openDevTools();
+    title: 'Green Tunnel',
+    frame: false,
+    transparent: true,
+    webPreferences: {
+      // Keep original functionality - minimal changes for compatibility
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: false
     }
+  });
+
+  // Save window state
+  stateManager.manage(win);
+
+  win.loadFile('./view/main-page/index.html');
+
+  win.once('ready-to-show', () => {
+    win.show();
+    win.focus();
+    turnOn();
+  });
+
+  win.on('closed', () => {
+    win = null;
+  });
+
+  if (debug) {
+    win.webContents.openDevTools();
+  }
 }
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('activate', () => {
-    if (win === null) {
-        createWindow();
-    }
+  if (win === null) {
+    createWindow();
+  }
 });
 
 app.whenReady().then(() => {
-    createWindow();
-    const iconPath = path.join(__dirname, 'images/iconTemplate@2x.png');
-    const trayIcon = nativeImage.createFromPath(iconPath);
-    tray = new Tray(trayIcon);
-    tray.setToolTip('Green Tunnel');
-    tray.setContextMenu(Menu.buildFromTemplate(menuItems));
+  createWindow();
+  const iconPath = path.join(__dirname, 'images/iconTemplate@2x.png');
+  const trayIcon = nativeImage.createFromPath(iconPath);
+  tray = new Tray(trayIcon);
+  tray.setToolTip('Green Tunnel');
+  tray.setContextMenu(Menu.buildFromTemplate(menuItems));
 
-    // Restore the window on double click
-    tray.on('double-click', () => {
-        if (win.isVisible()) {
-            win.hide();
-        } else {
-            win.show();
-        }
-    });
+  // Restore the window on double click
+  tray.on('double-click', () => {
+    if (win.isVisible()) {
+      win.hide();
+    } else {
+      win.show();
+    }
+  });
 })
 
 app.on('before-quit', async (e) => {
-    if (isOn) {
-        e.preventDefault();
-        await turnOff();
-        app.quit();
-    }
+  if (isOn) {
+    e.preventDefault();
+    await turnOff();
+    app.quit();
+  }
 });
 
 // Keep original IPC handlers - no changes to maintain compatibility
 ipcMain.on('close-button', (event, arg) => {
-    if (os.platform() === 'darwin') {
-        app.hide();
-    } else {
-        win.hide();
-    }
+  if (os.platform() === 'darwin') {
+    app.hide();
+  } else {
+    win.hide();
+  }
 });
 
 ipcMain.on('on-off-button', (event, arg) => {
-    if (isOn) {
-        turnOff();
-    } else {
-        turnOn();
-    }
+  if (isOn) {
+    turnOff();
+  } else {
+    turnOn();
+  }
 });
 
 // Optional: Single instance enforcement (can be removed if not needed)
 const gotTheLock = app.requestSingleInstanceLock();
 if (!gotTheLock) {
-    app.quit();
+  app.quit();
 } else {
-    app.on('second-instance', () => {
-        if (win) {
-            if (win.isMinimized()) win.restore();
-            win.focus();
-        }
-    });
+  app.on('second-instance', () => {
+    if (win) {
+      if (win.isMinimized()) win.restore();
+      win.focus();
+    }
+  });
 }
--- gui/view/main-page/scripts/main.js
+++ gui/view/main-page/scripts/main.js
@@ -1,30 +1,30 @@
 const { ipcRenderer } = require('electron');
 
-$( document ).ready(function() {
+$(document).ready(function () {
 
-    $('#close-button').on('click', () => {
-        ipcRenderer.send('close-button')
-    });
+  $('#close-button').on('click', () => {
+    ipcRenderer.send('close-button')
+  });
 
-    $('#on-off-button').on('click', () => {
-        ipcRenderer.send('on-off-button')
-    });
+  $('#on-off-button').on('click', () => {
+    ipcRenderer.send('on-off-button')
+  });
 
-    ipcRenderer.on('changeStatus', (event, isOn) => {
-        if(isOn) {
-            $('.toggle').each(() => {
-                $(this).find('*').removeClass('red');
-                $(this).find('*').addClass('green');
-            });
-            $('#status-off-on').html('is on');
-        } else {
-            $('.toggle').each(() => {
-                $(this).find('*').removeClass('green');
-                $(this).find('*').addClass('red');
-            });
-            $('#status-off-on').html('is off');
-        }
-    })
+  ipcRenderer.on('changeStatus', (event, isOn) => {
+    if (isOn) {
+      $('.toggle').each(() => {
+        $(this).find('*').removeClass('red');
+        $(this).find('*').addClass('green');
+      });
+      $('#status-off-on').html('is on');
+    } else {
+      $('.toggle').each(() => {
+        $(this).find('*').removeClass('green');
+        $(this).find('*').addClass('red');
+      });
+      $('#status-off-on').html('is off');
+    }
+  })
 
 });
 
